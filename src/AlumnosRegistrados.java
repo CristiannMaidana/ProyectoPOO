5cff6aaa80596835ba39aecb52e8c2fe29b8a2e3
CristiannMaidana <cristian.mg.david@gmail.com>
2024-08-24 23:40:30 -0300

    Se creo la clase AlumnosRegistrados.java, la cual sirve para verificar a los alumnos a la hora de iniciar sesion y se van guardando los alumnos ahi mediante se van creando en la ejecucion del programa.

---
+++ src/AlumnosRegistrados.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlumnosRegistrados {
+    private List<Alumnos> alumnosRegistrados = new ArrayList<Alumnos>();
+
+    public AlumnosRegistrados(Alumnos alumno) {
+        if (alumno != null)
+            alumnosRegistrados.add(alumno);
+    }
+
+    public Alumnos buscoAlumno(String nombreAlumno, String contrasenna){
+        Alumnos alumnoEncontrado, alumnoRegistrado;
+        alumnoEncontrado = null;
+        for (int i=0; i<alumnosRegistrados.size(); i++){
+            alumnoRegistrado = alumnosRegistrados.get(i);
+                if (alumnoRegistrado.getNombre().equals(nombreAlumno)) {
+                    if (alumnoRegistrado.getContrasenna().equals(contrasenna)) {
+                        alumnoEncontrado = alumnoRegistrado;
+                    }
+            }
+        }
+        return alumnoEncontrado;
+    }
+
+    public void add(Alumnos alumno){
+        alumnosRegistrados.add(alumno);
+    }
+}
