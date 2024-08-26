8f38f4af331b7b10c0f2c8443b0ee89550943a0a
CristiannMaidana <cristian.mg.david@gmail.com>
2024-08-25 22:05:56 -0300

    Se creo la nueva clase AlmacenCarreras.java, la cual sirve para almacenar las diferentes carreras que va creando el admin.

---
+++ src/AlmacenCarreras.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlmacenCarreras {
+    private List<Carreras> almacen = new ArrayList<>();
+
+    public AlmacenCarreras(Carreras carrera) {
+        if (carrera != null) {
+            add(carrera);
+        }
+    }
+
+    public void add (Carreras carrera) {
+        almacen.add(carrera);
+    }
+
+    public Carreras getCarreraPorNombre(String nombreCarrera){
+        for (Carreras carreras : almacen) {
+            if (carreras.getNombre().equals(nombreCarrera)) {
+                return carreras;
+            }
+        }
+        return null;
+    }
+
+    public Carreras getCarrera(int indice){
+        return almacen.get(indice);
+    }
+
+    public int getCantidadCarreras(){
+        return almacen.size();
+    }
+}
