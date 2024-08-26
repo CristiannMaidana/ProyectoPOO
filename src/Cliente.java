9c16ad1d3ab294889251e426cafd41fa47fcf0d8
CristiannMaidana <cristian.mg.david@gmail.com>
2024-08-25 22:38:01 -0300

    En la clase principal Cliente.java, se borro la propiedad carreras ya que no es utilizada con la nueva actualizacion, se agrego la propiedad almacenCarreras. En el main se agrego la funcionalidad de la creacion de materias a partir de la verificacion del logeo del admin que es el unico capaz de agregar carreras nuevas, siguiendo de su almacenamiento de vuelta de verificacion. En el metodo "cargoALumnosRegistrados" se agrego el perfil del admin. En el metodo "almacenoNuevoALumno" se borro el switch que servia para agregarle materias al alumno y se modifico el switch para corregir el plan de estudio del alumno. Se creo el nuevo metodo "ejecutoPanelCreoCarreras"

--- src/Cliente.java
+++ src/Cliente.java
@@ -2,90 +2,164 @@ import javax.swing.*;
 
 public class Cliente {
     static Alumnos alumno1;
-    static Carreras carreras;
     static AlumnosRegistrados cargoAlumnoNuevo;
+    static AlmacenCarreras almacenCarreras = new AlmacenCarreras(null);
 
     public static void main (String[]args) {
         cargoAlumnosRegistrados();//Cargo los alumnos en la clase "AlumnosRegistrados"
-
+        VCreoCarreras panelCreoCarreras;
+        //VperfilUsuario panelUsuario;
         Vlogeo panelLogearse = ejecutoPanelLogearse();
         while (panelLogearse.getBotones() || panelLogearse.getCrearUsuario()) {
-            if (panelLogearse.getCrearUsuario()) {
-                VInscripcion panelInscripcion = ejecutoPanelInscripcion();
-
-                if (panelInscripcion.getBoton())
-                    almacenoNuevoAlumno(panelInscripcion);
-
-                panelLogearse.crearUsuario = false;
-                panelLogearse = ejecutoPanelLogearse();
-            }
-
-            if (panelLogearse.getBotones()) {
-                alumno1 = cargoAlumnoNuevo.buscoAlumno(panelLogearse.getNombreUsuario(), panelLogearse.getContrasennaUsuario());
-
-                VperfilUsuario panelUsuario = ejecutoPanelUsuario();
-
-                if (panelUsuario.getCargoMasMaterias()) {
-                    VInscripcionMaterias panelCargoMaterias = ejecutoPanelInscripcionMaterias();
-
-                    if (!panelCargoMaterias.getBotonAceptar())
-                        if (!panelCargoMaterias.getBotonCancelar() || panelCargoMaterias.getBotonCargoNotas()) {
-                        VCargoNotas panelCargoNotas = ejecutoPanelCargoNotas();
-
-                        boolean repito = cargoMateriasNotasMuestroHistorial(panelCargoNotas, panelCargoMaterias);
-                        while (repito) {
-                            repito = cargoMateriasNotasMuestroHistorial(panelCargoNotas, panelCargoMaterias);
-                        }
-                    }
-                }
-                if (panelUsuario.getBoton()) {
+            //Si ingreso un admin creara carreras:
+            if (verificoAdmin(panelLogearse)){
+                panelCreoCarreras = ejecutoPanelCreoCarreras();
+                if (panelCreoCarreras.getBoton())
+                    creoCarrera(panelCreoCarreras);
+                else
                     panelLogearse = ejecutoPanelLogearse();
+            }
+            //Si no es admin o muestra perfil si se logea o crea usuario
+            else {
+                if (panelLogearse.getCrearUsuario()) {
+                    VInscripcion panelInscripcion = ejecutoPanelInscripcion();
+
+                    if (panelInscripcion.getBoton())
+                        almacenoNuevoAlumno(panelInscripcion);
+
+                    panelLogearse.crearUsuario = false;
+                    panelLogearse = ejecutoPanelLogearse();
+                }
+
+                if (verificoAdmin(panelLogearse)){
+                    panelCreoCarreras = ejecutoPanelCreoCarreras();
+                    if (panelCreoCarreras.getBoton())
+                        creoCarrera(panelCreoCarreras);
+                    else
+                        panelLogearse = ejecutoPanelLogearse();
+                }
+
+                if (panelLogearse.getBotones()) {
+                    alumno1 = cargoAlumnoNuevo.buscoAlumno(panelLogearse.getNombreUsuario(), panelLogearse.getContrasennaUsuario());
+
+                    VperfilUsuario panelUsuario = ejecutoPanelUsuario();
+
+                    if (panelUsuario.getCargoMasMaterias()) {
+                        VInscripcionMaterias panelCargoMaterias = ejecutoPanelInscripcionMaterias();
+
+                        if (!panelCargoMaterias.getBotonAceptar())
+                            if (!panelCargoMaterias.getBotonCancelar() || panelCargoMaterias.getBotonCargoNotas()) {
+                                VCargoNotas panelCargoNotas = ejecutoPanelCargoNotas();
+
+                                boolean repito = cargoMateriasNotasMuestroHistorial(panelCargoNotas, panelCargoMaterias);
+                                while (repito) {
+                                    repito = cargoMateriasNotasMuestroHistorial(panelCargoNotas, panelCargoMaterias);
+                                }
+                            }
+                    }
+                    if (panelUsuario.getBoton()) {
+                        panelLogearse = ejecutoPanelLogearse();
+                    }
                 }
             }
         }
     }
 
+    static boolean verificoAdmin(Vlogeo panelLogearse) {
+        if (panelLogearse.getNombreUsuario().equals("admin"))
+            return true;
+        else
+            return false;
+    }
+
+    static void creoCarrera(VCreoCarreras panelCarreras) {
+        int annio, cuatri;
+        String nombre, planDeEstudio;
+
+        annio = panelCarreras.getAnnioCarrera();
+        cuatri = panelCarreras.getCuatriCarrera();
+        nombre = panelCarreras.getNombreCarrera();
+        planDeEstudio = panelCarreras.getPlanEstudioCarrera();
+
+        Carreras newCarrera;
+        if (cuatri == 2) {
+             newCarrera= new Carreras(nombre, annio, 6) {
+                @Override
+                public float getCantMateriasOptativas() {
+                    return 0;
+                }
+            };
+        }else {
+            newCarrera = new Carreras(nombre, annio, 3) {
+                @Override
+                public float getCantMateriasOptativas() {
+                    return 0;
+                }
+            };
+        }
+
+        newCarrera.generoMaterias();
+
+        switch (planDeEstudio){
+            case "Plan A": {
+                newCarrera.setPlanDeEstudio(new PlanDeEstudioA(newCarrera));
+                break;
+            }
+            case "Plan B": {
+                newCarrera.setPlanDeEstudio(new PlanDeEstudioB(newCarrera));
+                break;
+            }
+            case "Plan C": {
+                newCarrera.setPlanDeEstudio(new PlanDeEstudioC(newCarrera,null));
+                break;
+            }
+            case "Plan D": {
+                newCarrera.setPlanDeEstudio(new PlanDeEstudioD(newCarrera,null));
+                break;
+            }
+            case "Plan E": {
+                newCarrera.setPlanDeEstudio(new PlanDeEstudioE(newCarrera,null));
+                break;
+            }
+        }
+
+        almacenCarreras.add(newCarrera);
+    }
+
     static void cargoAlumnosRegistrados (){
         cargoAlumnoNuevo = new AlumnosRegistrados(creoAlumnoSofia());
+        //admin 123456
+        Alumnos admin = new Alumnos("admin", 123456, "123456");
+        cargoAlumnoNuevo.add(admin);
     }
 
     static void almacenoNuevoAlumno (VInscripcion panelInscripcion){
         alumno1 = new Alumnos(panelInscripcion.getNombre() + " " + panelInscripcion.getApellido(),
                 panelInscripcion.getDni(), panelInscripcion.getContrasenna());
-        switch (panelInscripcion.getCarrera()) {
-            case "Licenciatura en sistemas.": {
-                alumno1.setCarrera(carreras = new CarreraSistemas(5, 6));
-                break;
-            }
-            case "Ingenieria Industrial.": {
-                alumno1.setCarrera(carreras = new CarreraSistemas(6, 6));
-                break;
-            }
-            case "Turismo.": {
-                alumno1.setCarrera(carreras = new CarreraPruebas(4, 6));
-                break;
-            }
-            default: break;
-        }//fin switch Inscripcion Carrera al alumno
+        alumno1.setCarrera(almacenCarreras.getCarreraPorNombre(panelInscripcion.getCarrera()));
         switch (panelInscripcion.getPlanElegido()){
-            case "Plan de estudio A": {
-                alumno1.getCarrera().setPlanDeEstudio(new PlanDeEstudioA(alumno1.getCarrera()));
+            case "Plan de estudio ¨A¨": {
+                alumno1.getCarrera().getPlanDeEstudio().setCarrera(alumno1.getCarrera());
+
                 break;
             }
-            case "Plan de estudio B": {
-                alumno1.getCarrera().setPlanDeEstudio(new PlanDeEstudioB(alumno1.getCarrera()));
+            case "Plan de estudio ¨B¨": {
+                alumno1.getCarrera().getPlanDeEstudio().setCarrera(alumno1.getCarrera());
                 break;
             }
-            case "Plan de estudio C": {
-                alumno1.getCarrera().setPlanDeEstudio(new PlanDeEstudioC(alumno1.getCarrera(),alumno1));
+            case "Plan de estudio ¨C¨": {
+                alumno1.getCarrera().getPlanDeEstudio().setCarrera(alumno1.getCarrera());
+                alumno1.getCarrera().getPlanDeEstudio().setAlumno(alumno1);
                 break;
             }
-            case "Plan de estudio D": {
-                alumno1.getCarrera().setPlanDeEstudio(new PlanDeEstudioD(alumno1.getCarrera(), alumno1));
+            case "Plan de estudio ¨D¨": {
+                alumno1.getCarrera().getPlanDeEstudio().setCarrera(alumno1.getCarrera());
+                alumno1.getCarrera().getPlanDeEstudio().setAlumno(alumno1);
                 break;
             }
-            case "Plan de estudio E": {
-                alumno1.getCarrera().setPlanDeEstudio(new PlanDeEstudioE(alumno1.getCarrera(), alumno1));
+            case "Plan de estudio ¨E¨": {
+                alumno1.getCarrera().getPlanDeEstudio().setCarrera(alumno1.getCarrera());
+                alumno1.getCarrera().getPlanDeEstudio().setAlumno(alumno1);
                 break;
             }
         }//fin switch Incripcion Plan de Estudio al alumno
@@ -175,7 +249,7 @@ public class Cliente {
     }
 
     static VInscripcion ejecutoPanelInscripcion(){
-        VInscripcion panelInscripcion = new VInscripcion();
+        VInscripcion panelInscripcion = new VInscripcion(almacenCarreras);
         panelInscripcion.setLocationRelativeTo(null);//Me ejecuta en el medio de la pantalla mi frame
         panelInscripcion.setVisible(true);
         hiloEspero(panelInscripcion);
@@ -204,5 +278,13 @@ public class Cliente {
         panelUsuario.setVisible(true);
         hiloEspero(panelUsuario);
         return panelUsuario;
+    }
+
+    static VCreoCarreras ejecutoPanelCreoCarreras(){
+        VCreoCarreras panelCarreras = new VCreoCarreras();
+        panelCarreras.setLocationRelativeTo(null);
+        panelCarreras.setVisible(true);
+        hiloEspero(panelCarreras);
+        return panelCarreras;
     }
 }
