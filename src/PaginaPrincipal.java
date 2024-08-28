d80cad57f45897b7c4119c44baa3637943f35c54
CristiannMaidana <cristian.mg.david@gmail.com>
2024-08-27 23:36:31 -0300

    En la clase PaginaPrincipal.java, se crearon variables booleanas para saber cual boton fue seleccionado, más sus respectivos listeners

--- src/PaginaPrincipal.java
+++ src/PaginaPrincipal.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PaginaPrincipal extends JFrame {
     private JLabel imagenUNTDFLabel;
@@ -10,10 +13,71 @@ public class PaginaPrincipal extends JFrame {
     private JLabel imagenSIULabel;
     private JPanel paginaPrincipal;
     private JLabel tituloPrincipal;
+    private boolean altaDeAlumnos = false, altaDeCarreras = false, altaDePlanDeEstudio = false, buscoAlumnos = false, modificoCarrera = false;
 
     public PaginaPrincipal() {
         setContentPane(paginaPrincipal);
         setSize(850,400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        altaDeAlumnosButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                altaDeAlumnos = true;
+                dispose();
+            }
+        });
+        altaDeCarrerasButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                altaDeCarreras = true;
+                dispose();
+            }
+        });
+        altaDePlanDeButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                altaDePlanDeEstudio = true;
+                dispose();
+            }
+        });
+        buscoAlumnoButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                buscoAlumnos = true;
+                dispose();
+            }
+        });
+        modificoCarreraButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                modificoCarrera = true;
+                dispose();
+            }
+        });
+    }
+
+    public boolean getAltaDeAlumnosButton() {
+        return altaDeAlumnos;
+    }
+
+    public boolean getAltaDeCarrerasButton() {
+        return altaDeCarreras;
+    }
+
+    public boolean getAltaDePlanDeButton() {
+        return altaDePlanDeEstudio;
+    }
+
+    public boolean getBuscoAlumnoButton() {
+        return buscoAlumnos;
+    }
+
+    public boolean getModificoCarreraButton() {
+        return modificoCarrera;
     }
 }
