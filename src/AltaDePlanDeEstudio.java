54d87be65d6c03756c58144dc310bca08f01ed84
CristiannMaidana <cristian.mg.david@gmail.com>
2024-08-29 23:36:15 -0300

    Se creo el form y clase de AltaDePlanDeEstudio.

---
+++ src/AltaDePlanDeEstudio.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class AltaDePlanDeEstudio extends JFrame {
+    private JPanel altaDePlanDeEstudio;
+    private JButton paginaPrincipalButton;
+    private JButton altaDeAlumnosButton;
+    private JButton altaDeCarrerasButton;
+    private JButton buscoAlumnosButton;
+    private JButton modificoCarreraButton;
+    private JComboBox comboBox1;
+    private JLabel descripcionPlanDeEstudio;
+    private JList listDeCarreras;
+    private JLabel nombreDeCarreraElegida;
+    private JButton asignarCarreraButton;
+    private JButton cancelarButton;
+    private JCheckBox checkBox1;
+
+    public AltaDePlanDeEstudio(){
+        listDeCarreras.setBorder(new LineBorder(Color.BLACK, 1)); // Color y grosor del borde
+
+        setContentPane(altaDePlanDeEstudio);
+        setUndecorated(true);
+        setLocationRelativeTo(null);
+        setSize(1000,500);
+
+        checkBox1.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                dispose();
+            }
+        });
+    }
+}
