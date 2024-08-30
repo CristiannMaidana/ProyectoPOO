2c0bd83437b93963e02eadcb8703bee977b820c4
CristiannMaidana <cristian.mg.david@gmail.com>
2024-08-29 23:07:22 -0300

    En la clase InscripcionCarreras.java, se agregaron variables "carreraElegida", "almacenCarreras" y "latch". La inicializacion en el constructor de estas dos ultimas. Los listeners a todos sus componentes y dos metodos "getCarreraElegida" y "cargoCarreras".

--- src/InscripcionCarreras.java
+++ src/InscripcionCarreras.java
@@ -1,27 +1,80 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
+import java.util.concurrent.CountDownLatch;
 
 public class InscripcionCarreras extends JFrame {
     private JPanel inscripcionCarrera;
-    private JComboBox comboBox1;
+    private JComboBox opcionesCarrerasComboBox;
     private JButton aceptarButton;
     private JButton cancelarButton;
     private JCheckBox checkBox1;
+    private String carreraElegida;
+    private AlmacenCarreras almacenCarreras;
+    private CountDownLatch latch;
 
-    public InscripcionCarreras(){
+    public InscripcionCarreras(AlmacenCarreras almacenCarreras, CountDownLatch latch) {
+        this.almacenCarreras = almacenCarreras;
+        this.latch = latch;
         setContentPane(inscripcionCarrera);
         setUndecorated(true);
         setLocationRelativeTo(null);
-        setSize(400,400);
+        setSize(300,150);
+        cargoCarreras();
 
         checkBox1.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
+                latch.countDown();
                 dispose();
             }
         });
+        opcionesCarrerasComboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                carreraElegida = (String) opcionesCarrerasComboBox.getSelectedItem();
+            }
+        });
+        aceptarButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                if (carreraElegida.isEmpty()){
+                    JOptionPane.showMessageDialog(null, "Debe elegir una carrera.");
+                }
+                else{
+                    int respuesta = JOptionPane.showConfirmDialog(null, "¿Esta seguro de elegir la carrera: "
+                            +carreraElegida+".?", "Aviso", JOptionPane.YES_NO_OPTION);
+                    if (respuesta == JOptionPane.YES_OPTION){
+                        latch.countDown();
+                        dispose();
+                    }
+                    else{
+                     cargoCarreras();
+                    }
+                }
+            }
+        });
+        cancelarButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                latch.countDown();
+                dispose();
+            }
+        });
+
+    }
+
+    public String getCarreraElegida() {
+        return carreraElegida;
+    }
+
+    private void cargoCarreras(){
+        opcionesCarrerasComboBox.removeAllItems();
+        opcionesCarrerasComboBox.addItem("prueba");
+        for(int i= 0; i<almacenCarreras.getCantidadCarreras(); i++){
+            opcionesCarrerasComboBox.addItem(almacenCarreras.getCarrera(i).getNombre());
+        }
     }
 }
