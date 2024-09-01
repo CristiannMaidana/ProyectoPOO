6ca2500d6fce7404a19c81527fa608bef38b1f37
CristiannMaidana <cristian.mg.david@gmail.com>
2024-09-01 02:39:08 -0300

    Se creo los archivos relacionados EditoDatosAlumnos.

---
+++ src/EditoDatosAlumnos.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class EditoDatosAlumnos extends JFrame {
+    private JPanel editarDatosAlumnos;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JPasswordField passwordField1;
+    private JButton guardarCambiosButton;
+    private JButton borrarAlumnoButton;
+    private JButton cancelarButton;
+    private JTextField textField3;
+    private JPasswordField passwordField2;
+
+    public EditoDatosAlumnos(){
+        setContentPane(editarDatosAlumnos);
+        setUndecorated(true);
+        setSize(300,300);
+    }
+}
