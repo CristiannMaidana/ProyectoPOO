98c1cce830bb643f2f8aecfc409c9f28e4050441
CristiannMaidana <cristian.mg.david@gmail.com>
2024-09-01 19:25:27 -0300

    En la clase AlmacenCarreras.java, se re organizo las variables y metodos correctamente.

--- src/AlmacenCarreras.java
+++ src/AlmacenCarreras.java
@@ -4,16 +4,25 @@ import java.util.List;
 public class AlmacenCarreras {
     private List<Carreras> almacen = new ArrayList<>();
 
+    //Constructor
     public AlmacenCarreras(Carreras carrera) {
         if (carrera != null) {
             add(carrera);
         }
     }
 
+    //Metodos void
     public void add (Carreras carrera) {
         almacen.add(carrera);
     }
+    public void borrarCarrera(int indice){
+        almacen.remove(indice);
+    }
 
+    //Metodos gets
+    public int getCantidadCarreras(){
+        return almacen.size();
+    }
     public Carreras getCarreraPorNombre(String nombreCarrera){
         for (Carreras carreras : almacen) {
             if (carreras.getNombre().equals(nombreCarrera)) {
@@ -22,16 +31,7 @@ public class AlmacenCarreras {
         }
         return null;
     }
-
     public Carreras getCarrera(int indice){
         return almacen.get(indice);
-    }
-
-    public int getCantidadCarreras(){
-        return almacen.size();
-    }
-
-    public void borrarCarrera(int indice){
-        almacen.remove(indice);
     }
 }
