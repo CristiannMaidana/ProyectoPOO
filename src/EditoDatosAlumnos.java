4cd31be2a4ccc54dea973d7d772f21ad7c69bdd1
CristiannMaidana <cristian.mg.david@gmail.com>
2024-09-01 13:56:33 -0300

    En la clase EditoDatosAlumnos.java, se agrego el semaforo como variable y en el constructor, se cambio el tamaño de la pagina. En el listener "borrarAlumnoButton", se agrego el semaforo y para cerrar la pagina. En el listener "cancelarButton", se agrego una alerta y su logica en base a al alerta. Se acomodaron los metodos "guardarCambios" y "hayCambios" en base al cambio de alumnos (apellido), se hiciieron 2 metodos "getHayCambios" y "getBorrarAlumno" que devuelven variables boolean

--- src/EditoDatosAlumnos.java
+++ src/EditoDatosAlumnos.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.concurrent.CountDownLatch;
 
 public class EditoDatosAlumnos extends JFrame {
     private JPanel editarDatosAlumnos;
@@ -16,16 +17,19 @@ public class EditoDatosAlumnos extends JFrame {
     private Alumnos usuario;
     private boolean hayCambiosDatos, borrarAlumno;
     private int alumnoAEliminar;
+    private CountDownLatch latch;
 
-    public EditoDatosAlumnos(Alumnos alumno) {
+    public EditoDatosAlumnos(Alumnos alumno, CountDownLatch latch) {
+        this.latch = latch;
         this.usuario = alumno;
         setContentPane(editarDatosAlumnos);
         setUndecorated(true);
-        setSize(300,300);
+        setSize(500,280);
         checkBox1.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
+                latch.countDown();
                 dispose();
             }
         });
@@ -42,7 +46,7 @@ public class EditoDatosAlumnos extends JFrame {
                     hayCambiosDatos = true;
                 }
                 else {
-                    JOptionPane.showMessageDialog(null, "No hay cambios por hacer,", "Aviso",
+                    JOptionPane.showMessageDialog(null, "No hay cambios por hacer.", "Aviso",
                             JOptionPane.INFORMATION_MESSAGE);
                 }
             }
@@ -57,6 +61,8 @@ public class EditoDatosAlumnos extends JFrame {
                     usuario.setCarrera(null);
                     borrarAlumno = true; // sabe que hacer la pagina principal con esto
                     alumnoAEliminar = usuario.getLegajo(); //Usa eso para borrar al alumno del registro
+                    latch.countDown();
+                    dispose();
                 }
                 else {
                     reseteoPagina();
@@ -67,8 +73,13 @@ public class EditoDatosAlumnos extends JFrame {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                reseteoPagina();
-                dispose();
+                int respuesta = JOptionPane.showConfirmDialog(null, "¿Cancelar edición?",
+                        "Confirmacion", JOptionPane.YES_NO_OPTION);
+                if (respuesta == JOptionPane.YES_OPTION) {
+                    reseteoPagina();
+                    latch.countDown();
+                    dispose();
+                }
             }
         });
     }
@@ -88,13 +99,9 @@ public class EditoDatosAlumnos extends JFrame {
         String nuevoAlumno = cambiarNombreTextField.getText(), nuevoApellido = cambiarApellidoTextField.getText(),
                 nuevoContrasenna = cambiarContrasennaPasswordField.getText(),
                 cambiarLegajo = cambiarLegajoTextField.getText(), actualNombre = usuario.getNombre(),
-                actualContrasenna = usuario.getContrasenna(), actualApellido;
+                actualContrasenna = usuario.getContrasenna(), actualApellido = usuario.getApellido();
         int actualLegajo= usuario.getLegajo();
 
-        //extraigo el apellido almacenado
-         actualApellido = actualNombre.substring(actualNombre.indexOf(" ") + 1);
-         //Dejo el nombre solo.
-         actualNombre = actualNombre.substring(0, actualNombre.indexOf(" "));
          if (!nuevoAlumno.equals(actualNombre) || !nuevoApellido.equals(actualApellido) ||
                  !nuevoContrasenna.equals(actualContrasenna) || !cambiarLegajo.equals(actualLegajo)){
              hayCambios = true;
@@ -108,23 +115,21 @@ public class EditoDatosAlumnos extends JFrame {
         String nuevoNombre = cambiarNombreTextField.getText(), nuevoApellido = cambiarApellidoTextField.getText(),
                 nuevoContrasenna = cambiarContrasennaPasswordField.getText(),
                 nuevoLegajo = cambiarLegajoTextField.getText(), actualNombre = usuario.getNombre(),
-                actualContrasenna = usuario.getContrasenna(), actualApellido;
+                actualContrasenna = usuario.getContrasenna(), actualApellido = usuario.getApellido(), actualLegajoTxt;
         int actualLegajo= usuario.getLegajo();
-        //extraigo el apellido almacenado
-        actualApellido = actualNombre.substring(actualNombre.indexOf(" ") + 1);
-        //Dejo el nombre solo.
-        actualNombre = actualNombre.substring(0, actualNombre.indexOf(" "));
+        actualLegajoTxt = Integer.toString(actualLegajo);
 
-        if (!nuevoNombre.equals(actualNombre)){
-            usuario.setNombre(nuevoNombre + actualApellido);
+
+        if (!nuevoNombre.equals(actualNombre) && !nuevoNombre.isEmpty()){
+            usuario.setNombre(nuevoNombre);
         }
-        if( !nuevoApellido.equals(actualApellido) ){
-            usuario.setNombre(actualNombre+nuevoApellido);
+        if( !nuevoApellido.equals(actualApellido ) && !nuevoApellido.isEmpty()){
+            usuario.setApellido(nuevoApellido);
         }
-        if (!nuevoContrasenna.equals(actualContrasenna)) {
+        if (!nuevoContrasenna.equals(actualContrasenna) && !nuevoContrasenna.isEmpty()) {
             usuario.setContrasenna(nuevoContrasenna);
         }
-        if(!nuevoLegajo.equals(actualLegajo)){
+        if(!nuevoLegajo.equals(actualLegajoTxt) && !nuevoLegajo.isEmpty()){
             usuario.setLegajo(Integer.parseInt(nuevoLegajo));
         }
     }
@@ -134,5 +139,13 @@ public class EditoDatosAlumnos extends JFrame {
         cambiarLegajoTextField.setText("");
         cambiarApellidoTextField.setText("");
         cambiarContrasennaPasswordField.setText("");
+    }
+
+    public boolean getHayCambios(){
+        return hayCambiosDatos;
+    }
+
+    public boolean getBorrarAlumno(){
+        return borrarAlumno;
     }
 }
