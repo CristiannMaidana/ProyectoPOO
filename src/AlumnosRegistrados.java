bab7516a73397a9522b9ad6d9a0959280f247a09
CristiannMaidana <cristian.mg.david@gmail.com>
2024-09-01 19:29:01 -0300

    En la clase AlumnosRegistrados.java, se re organizo las variables y metodos correctamente. Y se cambio el for en el metodo "buscoPorDNI"

--- src/AlumnosRegistrados.java
+++ src/AlumnosRegistrados.java
@@ -4,53 +4,41 @@ import java.util.List;
 public class AlumnosRegistrados {
     private List<Alumnos> alumnosRegistrados = new ArrayList<Alumnos>();
 
+    //Constructor
     public AlumnosRegistrados(Alumnos alumno) {
         if (alumno != null)
             alumnosRegistrados.add(alumno);
     }
 
-    public Alumnos buscoAlumno(String nombreAlumno, String contrasenna){
-        Alumnos alumnoEncontrado, alumnoRegistrado;
-        alumnoEncontrado = null;
-        for (int i=0; i<alumnosRegistrados.size(); i++){
-            alumnoRegistrado = alumnosRegistrados.get(i);
-                if (alumnoRegistrado.getNombre().equals(nombreAlumno)) {
-                    if (alumnoRegistrado.getContrasenna().equals(contrasenna)) {
-                        alumnoEncontrado = alumnoRegistrado;
-                    }
-            }
-        }
-        return alumnoEncontrado;
-    }
-
+    //Metodos void
     public void add(Alumnos alumno){
         alumnosRegistrados.add(alumno);
     }
-
-    public Alumnos buscoPorDNI(int dni){
-        Alumnos alumnoEncontrado = null, alumnoEnRegistro;
-        for (int i=0; i<alumnosRegistrados.size(); i++){
-            alumnoEnRegistro = alumnosRegistrados.get(i);
-            if (alumnoEnRegistro.getLegajo() == dni){
-                alumnoEncontrado = alumnoEnRegistro;
-            }
-        }
-        return alumnoEncontrado;
-    }
-
-    public int getSize(){
-        return alumnosRegistrados.size();
-    }
-
-    public Alumnos getAlumno(int i){
-        return alumnosRegistrados.get(i);
-    }
-
     public void remove(Alumnos alumno){
         for (int i=0; i<alumnosRegistrados.size(); i++){
             if (alumnosRegistrados.get(i) == alumno){
                 alumnosRegistrados.remove(i);
             }
         }
+    }
+
+    //Metodos gets
+    public int getSize(){
+        return alumnosRegistrados.size();
+    }
+    public Alumnos getAlumno(int i){
+        return alumnosRegistrados.get(i);
+    }
+
+
+    public Alumnos buscoPorDNI(int dni){
+        Alumnos alumnoEncontrado = null, alumnoEnRegistro;
+        for (Alumnos alumnosRegistrado : alumnosRegistrados) {
+            alumnoEnRegistro = alumnosRegistrado;
+            if (alumnoEnRegistro.getLegajo() == dni) {
+                alumnoEncontrado = alumnoEnRegistro;
+            }
+        }
+        return alumnoEncontrado;
     }
 }
