81ea3e429f5375af67f655c20dc1d262434a5541
CristiannMaidana <cristian.mg.david@gmail.com>
2024-09-01 19:50:52 -0300

    En la clase PlanDeEstudioA.java, se re organizo las variables y metodos correctamente.

--- src/PlanDeEstudioA.java
+++ src/PlanDeEstudioA.java
@@ -3,6 +3,26 @@ public class PlanDeEstudioA extends PlanDeEstudio{
         super(carrera);
     }
 
+    //Metodos get
+    @Override
+    public String getDescripion() {
+        return "En el plan de estudo 'A', tiene que tener aprobadas las cursadas de las correlativas para poder inscribirse.";
+    }
+
+    //Metodo set
+    @Override
+    public void setCarrera(Carreras carrera) {
+        this.carrera=carrera;
+    }
+    @Override
+    public void setAlumno(Alumnos alumno) {
+    }
+
+
+    @Override
+    public String toString() {
+        return "Plan de estudio ¨A¨";
+    }
     @Override
     public boolean aproboCorrelativas(Materias materias) {
         aprobo=true;
@@ -14,22 +34,4 @@ public class PlanDeEstudioA extends PlanDeEstudio{
         return aprobo;
     }
 
-    @Override
-    public void setCarrera(Carreras carrera) {
-        this.carrera=carrera;
-    }
-
-    @Override
-    public void setAlumno(Alumnos alumno) {
-    }
-
-    @Override
-    public String toString() {
-        return "Plan de estudio ¨A¨";
-    }
-
-    @Override
-    public String getDescripion() {
-        return "En el plan de estudo 'A', tiene que tener aprobadas las cursadas de las correlativas para poder inscribirse.";
-    }
 }
