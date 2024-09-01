470020ccb7d080fa8c5e96203b6eb0f4718e9b7d
CristiannMaidana <cristian.mg.david@gmail.com>
2024-09-01 19:58:25 -0300

    En la clase PlanDeEstudioD.java, se re organizo las variables y metodos correctamente.

--- src/PlanDeEstudioD.java
+++ src/PlanDeEstudioD.java
@@ -1,41 +1,13 @@
 public class PlanDeEstudioD extends PlanDeEstudio {
+    //Variables primitivas
     private byte cuatrimestreDeMateriaACursar =0, cantidadCuatrimestresRecorridos=0;;
+    //Objetos
     private Alumnos alumnos;
 
+    //Constructor
     public PlanDeEstudioD(Carreras carrera, Alumnos alumnos) {
         super(carrera);
         this.alumnos=alumnos;
-    }
-
-    @Override
-    public boolean aproboCorrelativas(Materias materias) {
-        boolean aprobo1 = true, aprobo2 = true;
-        aprobo1=aproboLaCursadaDeLasCorrelativas(materias, aprobo1);
-        if (aprobo1) {
-            aprobo2=buscoMateria(materias,aprobo2);
-        }
-        return (aprobo1 && aprobo2);
-    }
-
-    @Override
-    public void setCarrera(Carreras carrera) {
-        this.carrera=carrera;
-    }
-
-    @Override
-    public void setAlumno(Alumnos alumno) {
-        this.alumnos = alumno;
-    }
-
-    @Override
-    public String toString() {
-        return "Plan de estudio ¨D¨";
-    }
-
-    @Override
-    public String getDescripion() {
-        return "El plan de estudio 'D', tiene que tener aprobado las cursadas de las correlativas y los finales de todas" +
-                " las materias de 3 cuatrimestres previos al que se quiere anotar para poder inscribirse";
     }
 
     public boolean aproboLaCursadaDeLasCorrelativas (Materias materias, boolean aprobo1){
@@ -46,7 +18,6 @@ public class PlanDeEstudioD extends PlanDeEstudio {
         }
         return aprobo1;
     }
-
     public boolean buscoMateria (Materias materias, boolean aprobo2){
         cantidadCuatrimestresRecorridos = 0;
         for (byte i = 0; i < alumnos.getCarrera().getAnniosCarrera(); i++) {
@@ -125,5 +96,36 @@ public class PlanDeEstudioD extends PlanDeEstudio {
             else aprobo2=false;
         }
         return aprobo2;
+    }
+
+    //Metodos get
+    @Override
+    public String getDescripion() {
+        return "El plan de estudio 'D', tiene que tener aprobado las cursadas de las correlativas y los finales de todas" +
+                " las materias de 3 cuatrimestres previos al que se quiere anotar para poder inscribirse";
+    }
+
+    //Metodos set
+    @Override
+    public void setCarrera(Carreras carrera) {
+        this.carrera=carrera;
+    }
+    @Override
+    public void setAlumno(Alumnos alumno) {
+        this.alumnos = alumno;
+    }
+
+    @Override
+    public String toString() {
+        return "Plan de estudio ¨D¨";
+    }
+    @Override
+    public boolean aproboCorrelativas(Materias materias) {
+        boolean aprobo1 = true, aprobo2 = true;
+        aprobo1=aproboLaCursadaDeLasCorrelativas(materias, aprobo1);
+        if (aprobo1) {
+            aprobo2=buscoMateria(materias,aprobo2);
+        }
+        return (aprobo1 && aprobo2);
     }
 }
